81716c82f4543515ff36012c87fd29f238c0bf0c
Margaux Divernois <margaux.divernois@gmail.com>
2015-10-22 16:42:08 +0200

    Ajout du fichier AI.java

---
+++ src/Participants/BitterDivernois/AI.java
@@ -0,0 +1,11 @@
+package Participants.BitterDivernois;
+
+import Othello.Move;
+
+public class AI {
+
+	public static Move getBestMove(GameBoard gameBoard, int depth, int playerID)
+	{
+		return null;
+	}
+}
